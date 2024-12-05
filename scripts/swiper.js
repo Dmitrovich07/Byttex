4141bed806d3d8dc1d66ba65853e442b7dfed3a8
Olexandr <dmitrovich07O@gmail.com>
2024-12-05 02:32:02 +0200

    Add Swiper & Responsive

---
+++ scripts/swiper.js
@@ -0,0 +1,58 @@
+new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+new Swiper('.swiper-response', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1250: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+
+  },
+});
+
+new Swiper('.my-swiper', {
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+new Swiper('.step_cards-swiper', {
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
